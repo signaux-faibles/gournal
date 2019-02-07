749be8eb8324d900da2581aa6e7756b12b9d5da0
Christophe Ninucci <christophe@zbouboutchi.net>
2019-02-07 16:16:13 +0100

    ajustements

--- gournal.go
+++ gournal.go
@@ -2,19 +2,24 @@ package gournal
 
 // Tracker helper pour suivre les erreurs lors d'un traitement
 type Tracker struct {
-	Cycles int
-	Errors map[int][]string
+	Context map[string]string
+	Count   int
+	Errors  map[int][]string
+	Reports map[string]ReportFunction
 }
 
-// Ok incrémente le nombre de cycles ok
-func (tracker *Tracker) Ok() {
-	tracker.Cycles++
+// ReportFunction describes functions that are able to provide reports.
+type ReportFunction func(tracker Tracker) interface{}
+
+// Next step, increases count.
+func (tracker *Tracker) Next() {
+	tracker.Count++
 }
 
 // Error prend note des erreurs non nil
 func (tracker *Tracker) Error(err error) {
 	if err != nil {
-		cycleErrors, ok := tracker.Errors[tracker.Cycles]
+		cycleErrors, ok := tracker.Errors[tracker.Count]
 		if tracker.Errors == nil {
 			tracker.Errors = make(map[int][]string)
 		}
@@ -24,20 +29,43 @@ func (tracker *Tracker) Error(err error) {
 
 		cycleErrors = append(cycleErrors, err.Error())
 
-		tracker.Errors[tracker.Cycles] = cycleErrors
+		tracker.Errors[tracker.Count] = cycleErrors
 	}
 }
 
 // ErrorInCycle permet de savoir si des erreurs ont été constatées pendant le cycle
 func (tracker Tracker) ErrorInCycle() bool {
-	return len(tracker.Errors[tracker.Cycles]) > 0
+	return len(tracker.Errors[tracker.Count]) > 0
 }
 
-// CountErrors retourne le nombre d'erreurs du tracker
+// Report executes a reporting function and returns the report
+func (tracker Tracker) Report(code string) interface{} {
+	if _, ok := tracker.Reports[code]; ok {
+		return tracker.Reports[code](tracker)
+	} else {
+		return nil
+	}
+}
+
+// CountErrors returns total count of errors accounted since the start of the tracker
 func (tracker Tracker) CountErrors() int {
 	l := 0
 	for _, e := range tracker.Errors {
 		l += len(e)
 	}
 	return l
+}
+
+// CurrentErrors returns errors recorded during the cycle
+func (tracker Tracker) CurrentErrors() []string {
+	return tracker.Errors[tracker.Count]
+}
+
+// NewTracker provides a brand new tracker instance
+func NewTracker(context map[string]string, reportFunctions map[string]ReportFunction) Tracker {
+	tracker := Tracker{}
+	tracker.Reports = reportFunctions
+	tracker.Context = context
+	tracker.Errors = make(map[int][]string)
+	return tracker
 }
